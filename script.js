0a8beef6b78d717a896e07eeef6682631285e906
Marin Mema <marinmema@live.ca>
2025-01-21 10:04:57 -0500

    broadcast

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
+const webSocket = new WebSocket("https://marinm.net/broadcast");
+
 const container = document.getElementById("container");
 const bigHeart = document.getElementById("big-heart");
 
-bigHeart.addEventListener("click", newHeart);
+const animations = ["float-1", "float-2", "float-3", "float-4"];
+
+bigHeart.addEventListener("click", () => {
+	console.log("send");
+	webSocket.send(randomAnimation());
+});
 
 function randomAnimation() {
-	const animations = ["float-1", "float-2", "float-3", "float-4"];
-	return animations[
-		Math.floor(Math.random() * animations.length)
-	];
+    return animations[Math.floor(Math.random() * animations.length)];
 }
 
-function newHeart() {
-	const heart = document.createElement("div");
-	heart.classList.add("heart");
-	heart.textContent = "❤️";
-	heart.style.animationName = randomAnimation();
-	heart.style.animationDuration = "1500ms";
-	container.appendChild(heart);
-	heart.onanimationend = ({ target }) => target.remove();
+function newHeart(animation) {
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.textContent = "❤️";
+    heart.style.animationName = animation;
+    heart.style.animationDuration = "1500ms";
+    container.appendChild(heart);
+    heart.onanimationend = ({ target }) => target.remove();
 }
+
+webSocket.addEventListener("open", () => {
+    console.log("open");
+});
+
+webSocket.addEventListener("close", () => {
+    console.log("close");
+});
+
+webSocket.addEventListener("message", (event) => {
+	const animation = String(event.data);
+
+	if (animations.includes(animation)) {
+		newHeart(animation);
+	}
+});
