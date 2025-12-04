f52356cf8bcbdbfdec6ee781ea138f2dc0ecc6b7
Marin Mema <marinmema@live.ca>
2025-12-04 07:37:05 -0500

    useBroadcastWebSocket, copy styles

---
+++ src/hooks/useBroadcastWebSocket.ts
@@ -0,0 +1,123 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+export function useBroadcastWebSocket() {
+  const websocketRef = useRef<null | WebSocket>(null);
+  const [readyState, setReadyState] = useState<number>(WebSocket.CLOSED);
+  const [isOnline, setIsOnline] = useState<boolean>(window.navigator.onLine);
+  const [error, setError] = useState<boolean>(false);
+  const onMessageRef = useRef<(message: unknown) => void>(() => {});
+
+  function reasonError(method: string, reason: string) {
+    console.log(`ignoring ${method}() because ${reason}`);
+  }
+
+  const open = useCallback((url: string) => {
+    if (websocketRef.current !== null) {
+      reasonError("open", "already open");
+      return;
+    }
+    websocketRef.current = new WebSocket(url);
+
+    const websocket = websocketRef.current;
+
+    websocket.onopen = () => {
+      setReadyState(websocket.readyState);
+      setError(false);
+    };
+
+    websocket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      onMessageRef.current(message);
+    };
+
+    websocket.onclose = () => {
+      setReadyState(websocket.readyState);
+      websocketRef.current = null;
+    };
+
+    websocket.onerror = (err) => {
+      console.error(err);
+      setError(true);
+    };
+  }, []);
+
+  const send = useCallback((message: unknown) => {
+    if (websocketRef.current === null) {
+      reasonError("send", "closed");
+      return;
+    }
+    if (websocketRef.current.readyState != WebSocket.OPEN) {
+      reasonError("send", "closed");
+      return;
+    }
+    const stringified = safeJsonStringify(message);
+    if (stringified === null) {
+      reasonError("send", "message cannot be stringified");
+      return;
+    }
+    websocketRef.current.send(stringified);
+  }, []);
+
+  const close = useCallback(() => {
+    if (websocketRef.current === null) {
+      reasonError("close", "already closed");
+      return;
+    }
+
+    if (
+      websocketRef.current.readyState === WebSocket.CLOSING ||
+      websocketRef.current.readyState === WebSocket.CLOSED
+    ) {
+      reasonError("close", "already closed");
+      return;
+    }
+
+    if (websocketRef.current.readyState === WebSocket.CONNECTING) {
+      reasonError("close", "connecting in progress");
+      return;
+    }
+
+    if (websocketRef.current.readyState === WebSocket.OPEN) {
+      websocketRef.current.close();
+      return;
+    }
+
+    reasonError("close", "uncaught readyState");
+  }, []);
+
+  useEffect(() => {
+    const onOnline = () => setIsOnline(true);
+    const onOffline = () => setIsOnline(false);
+
+    window.addEventListener("online", onOnline);
+    window.addEventListener("offline", onOffline);
+
+    return () => {
+      close();
+      window.removeEventListener("online", onOnline);
+      window.removeEventListener("offline", onOffline);
+    };
+  }, [close]);
+
+  function onMessage(callback: (message: unknown) => void) {
+    onMessageRef.current = callback;
+  }
+
+  return {
+    isOnline,
+    readyState,
+    error,
+    open,
+    send,
+    close,
+    onMessage,
+  };
+}
+
+function safeJsonStringify(value: unknown): string | null {
+  try {
+    return JSON.stringify(value);
+  } catch {
+    return null;
+  }
+}
