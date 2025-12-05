9f9c163ebbcb702a5fbc8539578b291c8e14277f
Marin Mema <marinmema@live.ca>
2025-12-05 08:47:33 -0500

    useTimeoutState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { useEffect, useCallback } from "react";
 import { useBroadcastWebSocket } from "./hooks/useBroadcastWebSocket";
+import { useTimeoutState } from "./hooks/useTimeoutState";
 
 function App() {
   const webSocket = useBroadcastWebSocket();
+  const [tapped, setTappedWithTimeout] = useTimeoutState(false, 555);
 
   const channel =
     new URL(window.location.href).searchParams.get("channel") ?? "";
@@ -17,15 +19,23 @@ function App() {
 
   useEffect(() => webSocket.open(SERVER_URL), [webSocket, SERVER_URL]);
 
+  function tap() {
+    setTappedWithTimeout(true);
+  }
+
   return (
     <div>
-      <div
-        className={webSocket.readyState !== WebSocket.OPEN ? "grayscale" : ""}
-      >
+      <div>
         <svg
           id="big-heart"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 512 512"
+          className={
+            (webSocket.readyState !== WebSocket.OPEN ? "grayscale" : "") +
+            " " +
+            (tapped ? "tapped" : "")
+          }
+          onClick={tap}
         >
           {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
           <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
---
+++ src/hooks/useTimeoutState.ts
@@ -0,0 +1,19 @@
+import { useRef, useState } from "react";
+
+export function useTimeoutState<T>(
+  initialState: T,
+  timeout?: number,
+): [T, (value: T) => void] {
+  const [state, setState] = useState<T>(initialState);
+  const timeoutIdRef = useRef<null | number>(null);
+
+  function setStateWithTimeout(value: T) {
+    setState(value);
+    if (timeoutIdRef.current !== null) {
+      clearTimeout(timeoutIdRef.current);
+    }
+    timeoutIdRef.current = setTimeout(() => setState(initialState), timeout);
+  }
+
+  return [state, setStateWithTimeout];
+}
